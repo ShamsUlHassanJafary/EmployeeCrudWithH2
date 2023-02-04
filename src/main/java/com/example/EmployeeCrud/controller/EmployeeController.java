cd788e35e651b5e7e85b05bd6d3df16eece95d55
ShamsUlHassanJafary <shamskhosa7@gmail.com>
2023-02-04 12:49:41 -0800

    first commit

---
+++ src/main/java/com/example/EmployeeCrud/controller/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.example.EmployeeCrud.controller;
+
+import com.example.EmployeeCrud.model.Employee;
+import com.example.EmployeeCrud.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee findById(@PathVariable Long id) {
+        return employeeService.findById(id);
+    }
+
+    @PostMapping
+    public Employee save(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+}
+
---
+++ src/main/java/com/example/EmployeeCrud/model/Employee.java
@@ -0,0 +1,22 @@
+package com.example.EmployeeCrud.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    // other fields and methods
+}
